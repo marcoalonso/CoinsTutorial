d35ee786f1efdf5ff76f97a0d1c97b2c0e6981f7
Marco Alonso <marcoalonso@192.168.1.21>
2024-03-18 19:59:43 -0600

    Module 01 Project Setup

---
+++ CoinsTutorial/App/CoinsTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinsTutorialApp.swift
+//  CoinsTutorial
+//
+//  Created by Marco Alonso on 18/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinsTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ CoinsTutorial/Core/Home/Views/HomeView.swift
@@ -0,0 +1,26 @@
+//
+//  HomeView.swift
+//  CoinsTutorial
+//
+//  Created by Marco Alonso on 18/03/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                
+                // top movers view
+                
+                // all coins view
+            }
+            .navigationTitle("Coins Prices")
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
